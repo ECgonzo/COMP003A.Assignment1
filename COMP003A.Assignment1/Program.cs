b3a81e03b36a859b27a8a72721e0a83e50cfb137
gonzo <gonzo@DESKTOP-4G8ROUF>
2024-01-21 18:05:47 -0800

    Add project files.

---
+++ COMP003A.Assignment1/Program.cs
@@ -0,0 +1,114 @@
+﻿/* 
+ * Author:      Ethen Gonzalez
+ * Course:      COMP-003A-L01
+ * Purpose:     Code for Assignment 1
+ */
+namespace COMP0031.Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("100 Things About Me");
+            Console.WriteLine("1. My name is Ethen Gonzalez");
+            Console.WriteLine("2. My middle name is my grandpa and dad's name");
+            Console.WriteLine("3. My middle name is Cristobal");
+            Console.WriteLine("4. My dad is Mexican");
+            Console.WriteLine("5. My Mom is Portuguese");
+            Console.WriteLine("6. I went to Lemoore High");
+            Console.WriteLine("7. I graduated in 2022");
+            Console.WriteLine("8. My major is Computer Science");
+            Console.WriteLine("9. I go to the gym");
+            Console.WriteLine("10. I was born in Hanford");
+            Console.WriteLine("11. I live in Lemoore");
+            Console.WriteLine("12. My younger sisters name is Hana");
+            Console.WriteLine("13. My older sisters name is Haley");
+            Console.WriteLine("14. My older sister lives in Dallas");
+            Console.WriteLine("15. I am 20 years old");
+            Console.WriteLine("16. I played Football in Highschool");
+            Console.WriteLine("17. I did Wrestling in Highschool");
+            Console.WriteLine("18. I've injured my ACL");
+            Console.WriteLine("19. I enjoy Fall the most");
+            Console.WriteLine("20. My birthday is in November");
+            Console.WriteLine("21. I'm afraid of deep water");
+            Console.WriteLine("22. I'm afraid of spiders");
+            Console.WriteLine("23. Christmas is my favorite Holiday");
+            Console.WriteLine("24. I watch podcasts on youtube");
+            Console.WriteLine("25. My dogs name is Blue");
+            Console.WriteLine("26. Blue is a Bernese Mountain Dog and Poodle Mix");
+            Console.WriteLine("27. My favorite boardgame is the Game of Life");
+            Console.WriteLine("28. I like pepperoni and olives on pizza");
+            Console.WriteLine("29. I like vanilla ice cream");
+            Console.WriteLine("30. I know how to make ice cream");
+            Console.WriteLine("31. My favorite candy is a Reeses");
+            Console.WriteLine("32. My favorite genre is Action/Adventure");
+            Console.WriteLine("33. I like rainy weather");
+            Console.WriteLine("34. I've been to New York");
+            Console.WriteLine("35. I've been to Washington DC");
+            Console.WriteLine("36. I've been to the Grand Canyon");
+            Console.WriteLine("37. I've been to Mount Rushmoure");
+            Console.WriteLine("38. I've been to Alcatraz");
+            Console.WriteLine("39. My favorite game right now is Baldur's Gate 3");
+            Console.WriteLine("40. I was introduced to gaming by my grandpa");
+            Console.WriteLine("41. I watched my grandpa play Oblivion");
+            Console.WriteLine("42. Skyrim is one of my favorite games");
+            Console.WriteLine("43. I like to play Minecraft");
+            Console.WriteLine("44. I like to watch YouTube");
+            Console.WriteLine("45. I like to scroll on TikTok");
+            Console.WriteLine("46. I have instagram");
+            Console.WriteLine("47. I rarely use Snapchat");
+            Console.WriteLine("48. I would like to learn a fighting style");
+            Console.WriteLine("49. I like to watch/read anime");
+            Console.WriteLine("50. I like pizza");
+            Console.WriteLine("51. I like spagetti");
+            Console.WriteLine("52. I wish to be able to support my family");
+            Console.WriteLine("53. My team is the 49ers");
+            Console.WriteLine("54. Nick Bosa is my player for the Niners");
+            Console.WriteLine("55. I enjoy doing landscaping");
+            Console.WriteLine("56. I went to Stratford Elementary");
+            Console.WriteLine("57. My favorite color is orange");
+            Console.WriteLine("58. I have family in Oklahoma");
+            Console.WriteLine("59. My family and I once drove to OK 23 hours straight");
+            Console.WriteLine("60. I like spending time with family");
+            Console.WriteLine("61. I enjoy doing curls in the gym the most");
+            Console.WriteLine("62. I don't drink coffee");
+            Console.WriteLine("63. I try to drink 2 gallons of water a day");
+            Console.WriteLine("64. I want to go to iceland");
+            Console.WriteLine("65. I want to travel Europe");
+            Console.WriteLine("66. I listen to Rock music");
+            Console.WriteLine("67. I listen to Rap");
+            Console.WriteLine("68. I like Keanu Reeves");
+            Console.WriteLine("69. The last movie I watched was Trolls 3");
+            Console.WriteLine("70. My favorite book series is the Blood of Olympus");
+            Console.WriteLine("71. I enjoy eating tamales on Christmas");
+            Console.WriteLine("72. I like using my PC");
+            Console.WriteLine("73. I've been to the Smithsonian Museum in NY");
+            Console.WriteLine("74. I like dark humor");
+            Console.WriteLine("75. I like laying next to my dog");
+            Console.WriteLine("76. I'd like to get a tattoo in the future");
+            Console.WriteLine("77. I love Mexican food");
+            Console.WriteLine("78. I worked as a caterer");
+            Console.WriteLine("79. I volunteered at a soup kitchen");
+            Console.WriteLine("80. I spend about 2 hours at the gym");
+            Console.WriteLine("81. I go to the gym with my younger sister");
+            Console.WriteLine("82. I like Breaking Bad");
+            Console.WriteLine("83. My favorite anime is One Piece");
+            Console.WriteLine("84. I also like JoJo's Bizarre Adventure");
+            Console.WriteLine("85. I understand only a little Spanish");
+            Console.WriteLine("86. I have a 1978 K5 Chevy Blazer");
+            Console.WriteLine("87. My main car is a 2014 Mazda");
+            Console.WriteLine("88. I am afraid of heights");
+            Console.WriteLine("89. I have asthma");
+            Console.WriteLine("90. My mom had lung cancer");
+            Console.WriteLine("91. I like to watch cartoons from when I was younger");
+            Console.WriteLine("92. I like the snow");
+            Console.WriteLine("93. My family goes to Shaver Lake every year together");
+            Console.WriteLine("94. I like John Cena");
+            Console.WriteLine("95. I have a PC");
+            Console.WriteLine("96. I have a Xbox");
+            Console.WriteLine("98. I have a PS5");
+            Console.WriteLine("99. I'd like to visit Italy");
+            Console.WriteLine("100. I like Legos");
+        }
+    }
+}
